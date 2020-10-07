768683c52d7debfe755810288a89db06bd1586a4
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2020-10-07 14:02:22 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  experiments      = [sensitive_variables]
+  required_version = ">=0.14.0-alpha20201007"
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+  default   = "hunter2"
+}
+
+resource "random_pet" "pet" {
+  prefix = title(var.password)
+}
+
+resource "null_resource" "none" {
+  triggers = {
+    secret = var.password
+  }
+}
